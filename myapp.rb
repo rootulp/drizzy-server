96bad55d27a33a3d2b32157fd45c49fab010c42e
Rootul Patel <rootulp@palantir.com>
2017-03-03 23:42:42 +0000

    Fetch a random lyric

--- myapp.rb
+++ myapp.rb
@@ -1,7 +1,18 @@
 # myapp.rb
 require 'sinatra'
 
+lyrics = [
+  "I can't really see another squad tryna cross us",
+  "Nobody really likes us except for us",
+  "Whole squad on that real shit",
+  "No new friends",
+  "I live for the nights that I can't remember with the people that I won't forget",
+  "My team good, we don't really need a mascot",
+  "Pussy run everything",
+  "I still ride with my day one",
+  "You the best I ever had"
+]
+
 get '/lyric' do
-  "I'm lookin' at they first week numbers like what are those/" +
-  "I mean you boys not even coming close"
+  lyrics.sample()
 end
